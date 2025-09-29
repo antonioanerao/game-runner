b60fa2914c41078c6b55e8bc71445b071bc7c6c8
Antonio Anerão <anerao.junior@gmail.com>
2025-09-29 05:08:29 -0500

    add: rec and collision

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 font_test = pygame.font.Font('./font/Pixeltype.ttf', size=50)
 
-sky_surface = pygame.image.load('./graphics/Sky.png')
-ground_surface = pygame.image.load('./graphics/ground.png')
-text_surface = font_test.render('Runner', False, 'Black')
-
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_x_position = 800
 snail_speed = 4
+sky_surf = pygame.image.load('./graphics/Sky.png').convert()
+ground_surf = pygame.image.load('./graphics/ground.png').convert()
+text_surf = font_test.render('Runner', False, 'Black')
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rec = snail_surf.get_rect(midbottom=(800, 300))
+
+
+player_surf = pygame.image.load('./graphics/Player/player_walk_1.png').convert_alpha()
+player_rec = player_surf.get_rect(midbottom=(80, 300))
+
 
 while True:
     for event in pygame.event.get():
@@ -22,14 +27,21 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (350, 50))
-    snail_x_position -= snail_speed
-    if snail_x_position < -100:
-        snail_x_position = 800
-        snail_speed += 1
-    screen.blit(snail_surface, (snail_x_position, 265))
+    speed_surf = font_test.render('Speed: ' + str(round(snail_speed, 2)), False, 'Black')
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 300))
+    screen.blit(text_surf, (350, 50))
+    screen.blit(speed_surf, (350, 80))
+
+    snail_rec.x -= 4
+    if snail_rec.right <= 0:
+        snail_rec.left = 800
+        snail_speed += 0.1
+    screen.blit(snail_surf, snail_rec)
+    screen.blit(player_surf, player_rec)
+
+    if player_rec.colliderect(snail_rec):
+        print("collision")
 
     pygame.display.update()
     clock.tick(60)
